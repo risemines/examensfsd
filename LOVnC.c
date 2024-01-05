e6676bb05e59355e350daafa4f49124e57bbb993
Yasmine <jsyas04@gmail.com>
2024-01-05 13:42:43 +0100

    change 2

--- LOVnC.c
+++ LOVnC.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct enreg{
-  char bloc[100];
-}enreg;
+typedef struct fbloc{
+  char tab[100];
+  int key;
+  int svt;
+};
+int recherche(FILE *fichier, char nom){
+  struct fbloc bloc;
+  int i=0;
+  while (fread(&bloc, sizeof(struct fbloc),1,fichier) == 1){
+    i++;
+    if(strncmp(bloc.tab[i], nom, strlen(nom))){
+      printf("bloc trouve: %s avec sa cle %d ",bloc.tab[i], bloc.key);
+      return 1;
+    }
+  }
+  printf("bloc non trouve");
+  return 0;
+}  
+int  main(){
+  FILE *fichier;
+  struct bloc buffer;
+fichier = fopen("","");
+
+if (fichier == NULL){
+printf("erreur");
+return 1;
+}
+}
