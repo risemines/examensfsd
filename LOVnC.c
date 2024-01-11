d3161a0adea88dada07814ea1dd65c05a8f5fdfd
Yasmine <jsyas04@gmail.com>
2024-01-11 20:56:11 +0100

    changes

--- LOVnC.c
+++ LOVnC.c
@@ -1,58 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#define false 
+#define true 1
 
-typedef struct fbloc{
+
+typedef struct Tbloc{
   char tab[100];
-  int key;
   int svt;
-};
+  int prd;
+}Tbloc;
+
+typedef Tbloc Buffer;
+
+typedef struct Etudiant{
+  char nom[25];
+  char prenom[25];
+  char key[4]; //pr la recherche
+  char taille[4];
+}Etudiant;
 
 typedef struct Entete{
-  int nb_bloc;
-  int compteur;
-};
+  int premier; //premier bloc
+  int insert; // nb de caracteres inseres
+  int sup; // nb de caractere supprimes
+  int nbloc; // nb de blocs
+}Entete;
 
-
-int recherche(FILE *fichier, char nom){
-  struct fbloc bloc;
-  int i=0;
-  while (fread(&bloc, sizeof(struct fbloc),1,fichier) == 1){
-    i++;
-    if(strncmp(bloc.tab[i], nom, strlen(nom))){
-      printf("bloc trouve: %s avec sa cle %d ",bloc.tab[i], bloc.key);
-      return 1;
-    }
-  }
-  printf("bloc non trouve");
-  return 0;
-}  
+typedef struct LOV{
+  FILE *fichier;
+  Entete entete;
+}LOV;
 
 
 void lire(FILE *fichier, int i, struct fbloc *buffer){
-  fread(buffer,sizeof(struct fbloc),1,fichier);
+  fread(buffer,sizeof(struct Tbloc),1,fichier);
 }
 
 void ecrire(FILE *fichier,int i, struct fbloc *buffer){
-  fwrite(buffer, sizeof(struct fbloc), 1, fichier);
+  fwrite(buffer, sizeof(struct Tbloc), 1, fichier);
 }
 
 int recupentete(FILE *fichier, int i){
   struct Entete entete;
   fread(&entete, sizeof(struct Entete),1,fichier);
-  if(i==1){
-    return entete.nb_bloc;
+  switch(i){
+   case 1: return entete.premier;
+   break;
+   case 2: return entete.insert;
+   break;
+   case 3: return entete.sup;
+   break;
+   case 4: return entete.nbloc;
+   break;
   }
-  else if(i==2){
-    return entete.compteur;
-  }
-  else{
-printf("chiffre invalide");
-return -1;}
 }
 
 int  main(){
   FILE *fichier;
-  struct fbloc buffer;
 fichier = fopen("","");
 
 if (fichier == NULL){
